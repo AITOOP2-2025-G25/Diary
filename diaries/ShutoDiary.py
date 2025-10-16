21eaf99d6cace4fe7ee1b3620bd15ca214ecc5a3
Shuto0126 <k24013kk@aitech.ac.jp>
2025-10-16 15:18:35 +0900

    Create ShutoDiary.py

---
+++ diaries/ShutoDiary.py
@@ -0,0 +1,11 @@
+from diaries.AbstractDiary import AbstractDiary
+class ShutoDiary(AbstractDiary):
+
+    def get_date(self):
+        return "2025-10-16"
+    
+    def get_summary(self):
+        return "なにもない一日だった"
+    
+    def get_author(self):
+        return "Sample"
