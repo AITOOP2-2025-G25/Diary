71c57f528d7ce60565fc1abe89694cbc913385cd
sinnoua4 <k24052kk@aitech.ac.jp>
2025-10-16 15:47:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from diaries.DiarySample import DiarySample
-from diaries.KannouDiary import DiarySample
+
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), KannouDiary.py()],
+diaries = [DiarySample(), ]
 
 for d in diaries:
     print("---------------------------------")
