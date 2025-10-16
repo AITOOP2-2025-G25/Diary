2c77589afee25cc5c734476de0dcfc4372c7dd6a
k24046 <k24046kk@aitech.ac.jp>
2025-10-16 16:01:46 +0900

    main直した

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from diaries.DiarySample import DiarySample
+from diaries.KaneiwaDiary import DiarySample
+from diaries.KannouDiary import KannouDiary
+from diaries.NomuraDiary import DiarySample
+from diaries.ShutoDiary import ShutoDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [DiarySample(), 
+           DiarySample(), 
+           KannouDiary(), 
+           DiarySample(), 
+           ShutoDiary(), ]
+
 
 for d in diaries:
     print("---------------------------------")
