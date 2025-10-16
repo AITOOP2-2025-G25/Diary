5a7d079a66fd48dce479a59b2a96430126dc5175
shun-NMR <nomurashiyun@gmail.com>
2025-10-16 15:47:51 +0900

    野村日記修正

--- diaries/NomuraDiary.py
+++ diaries/NomuraDiary.py
@@ -5,7 +5,7 @@ class DiarySample(AbstractDiary):
         return "2025-10-16"
     
     def get_summary(self):
-        return "GItややこしい。あと、リーダーの負担が重い!"
+        return "Gitややこしい。あと、リーダーの負担が重い!"
     
     def get_author(self):
         return "Nomura"
