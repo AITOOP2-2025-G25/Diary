eac225b2a8047cab9d715c48cec10db618a061f7
k24046 <k24046kk@aitech.ac.jp>
2025-10-16 15:43:14 +0900

    カネイワ日記の追加

---
+++ diaries/KaneiwaDiary.py
@@ -0,0 +1,11 @@
+from diaries.AbstractDiary import AbstractDiary
+class DiarySample(AbstractDiary):
+
+    def get_date(self):
+        return "2025-10-16"
+    
+    def get_summary(self):
+        return "Gitすごくむずい"
+    
+    def get_author(self):
+        return "Kaneiwa"
